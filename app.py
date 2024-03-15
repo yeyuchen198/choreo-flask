1f1347587af7575fc770ae490f0b0e81d066efd2
yeyuchen198 <89305403+yeyuchen198@users.noreply.github.com>
2024-03-15 16:26:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -197,7 +197,7 @@ def sh_str(id):
 
 # app.run(host='0.0.0.0', port=int(os.getenv('PORT') or '8080'))
 # app.run(host='0.0.0.0', port=int(os.getenv('PORT') or '3000'))
-app.run(host='0.0.0.0', port=3000)
+app.run(host='0.0.0.0', port=5000)
 
 
 
